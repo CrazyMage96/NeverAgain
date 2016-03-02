0dbbe15b54f64667492e4085482b5656210b3591
CrazyMage96 <bloch@gepraktikum32.ds.rbg.tum.de>
2016-03-02 12:34:29 +0100

    Pathfinding

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	public Transform goal;
+	
+	void Update () {
+		NavMeshAgent agent = GetComponent<NavMeshAgent>();
+		agent.destination = goal.position; 
+	}
+}
