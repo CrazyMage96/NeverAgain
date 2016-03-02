4880a98a6e9abc2d9e20ca65800d423796e99059
MedeaSpr <springme@gepraktikum33.ds.rbg.tum.de>
2016-03-02 18:39:31 +0100

    WallBehaviour
    Wand verschwiendet so das man durchgehen kann

---
+++ Assets/Scripts/WallBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallBehaviour : MonoBehaviour {
+
+	private Renderer rend;
+	private Collider coll;
+
+	public float time = 2f;
+	// Use this for initialization
+	void Start () {
+		rend = gameObject.GetComponent<Renderer>();
+		coll = gameObject.GetComponent<Collider> ();
+		StartCoroutine (blinken(time));
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+	}
+
+	IEnumerator blinken(float ptime)
+	{
+		while (true) {
+			yield return new WaitForSeconds (ptime);
+			//gameObject.enabled = false;
+			coll.enabled = false;
+			rend.enabled = false;
+			yield return new WaitForSeconds (ptime);
+			coll.enabled = true;
+			rend.enabled = true;
+		}
+	}
+
+}
