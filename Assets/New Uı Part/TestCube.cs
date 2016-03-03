fd20f397ae27ee8debd183db0dfb45880eac1767
Wizard0f0z <oguzdagasan1996@gmail.com>
2016-03-03 14:34:26 +0100

    uı part

---
+++ Assets/New Uı Part/TestCube.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestCube : MonoBehaviour {
+	
+	void Start () {
+		ScoreManager.score = 5;
+
+
+	}
+	
+
+	void Update () {
+		ScoreManager.score = 5;
+	}
+}
