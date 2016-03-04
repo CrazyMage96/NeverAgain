7644f5d2f252b175b883cf1aeb2f4756773aafa8
CrazyMage96 <bloch@ds.rbg.tum.de>
2016-03-04 12:03:01 +0100

    added zombi kill button

---
+++ Assets/Scripts/ZombiButton.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombiButton : MonoBehaviour {
+	private Renderer renderer;
+	private Collider collider;
+	private NavMeshObstacle nav;
+	private GameObject[] spawnedZombies;
+
+	// Use this for initialization
+	void Start () {
+		renderer = gameObject.GetComponent<Renderer>();
+		collider = gameObject.GetComponent<Collider> ();
+		nav = gameObject.GetComponent<NavMeshObstacle>();
+
+		/*int random = (int)Random.Range (0f, 3f);
+		if (random != 0) {
+			renderer.enabled = false;
+			collider.enabled = false;
+			nav.enabled = false;
+		}*/
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision otherObject)
+	{
+		if(otherObject.gameObject.tag == "Player")
+		{
+			collider.enabled = false;
+			renderer.enabled = false;
+			nav.enabled = false;
+
+			spawnedZombies = GameObject.FindGameObjectsWithTag("Enemy");
+			foreach(GameObject zombi in spawnedZombies){
+				Destroy(zombi);
+			}
+		}
+	}
+}
