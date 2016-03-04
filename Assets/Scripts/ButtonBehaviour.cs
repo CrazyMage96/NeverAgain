ff93563a5be5d28ce2034dbdc455ff916f11f40b
CrazyMage96 <bloch@ds.rbg.tum.de>
2016-03-04 13:15:08 +0100

    Help

--- Assets/Scripts/ButtonBehaviour.cs
+++ Assets/Scripts/ButtonBehaviour.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class ButtonBehaviour : MonoBehaviour {
 
+	private GameObject startGame;
+	private GameObject helpText;
+
 	public void LoadLevelByName(string levelName)
 	{
 		Application.LoadLevel(levelName);
@@ -11,9 +14,24 @@ public class ButtonBehaviour : MonoBehaviour {
 	
 	}
 
+	public void HelpButtonClicked(){
+		UnityEngine.UI.Text text = GameObject.Find ("Canvas/Help/Text").GetComponent<UnityEngine.UI.Text> ();
+		if (text.text == "Help") {
+			text.text = "Back";
+			startGame.SetActive (false);
+			helpText.SetActive (true);
+		} else {
+			text.text = "Help";
+			startGame.SetActive (true);
+			helpText.SetActive (false);
+		}
+	}
+
 	// Use this for initialization
 	void Start () {
-	
+		startGame = GameObject.Find ("Canvas/Start Game");
+		helpText = GameObject.Find ("Canvas/helpText");
+		helpText.SetActive (false);
 	}
 	
 	// Update is called once per frame
